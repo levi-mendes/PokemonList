8a127a40f1d299e16aef0855080127bba43a2057
Levi Mendes <levi.mendes@foursys.com.br>
2024-06-11 23:11:05 -0300

    refactoring / melhorias

--- app/src/main/java/com/example/pokemonappagilecontent/navigation/PokemonAppDestinations.kt
+++ app/src/main/java/com/example/pokemonappagilecontent/navigation/PokemonAppDestinations.kt
@@ -1,37 +1,6 @@
 package com.example.pokemonappagilecontent.navigation
 
-import androidx.navigation.NavGraph.Companion.findStartDestination
-import androidx.navigation.NavHostController
-import com.example.core.list.PokemonItemEntity
-
 object PokemonAppDestinations {
     const val LIST = "list"
     const val DETAILS = "details/{name}"
-}
-
-class PokemonAppNavigationActions(navController: NavHostController) {
-    val navigateToList: () -> Unit = {
-        navController.navigate(PokemonAppDestinations.LIST) {
-            // Pop up to the start destination of the graph to
-            // avoid building up a large stack of destinations
-            // on the back stack as users select items
-            popUpTo(navController.graph.findStartDestination().id) {
-                saveState = true
-            }
-            // Avoid multiple copies of the same destination when
-            // reselecting the same item
-            launchSingleTop = true
-            // Restore state when reselecting a previously selected item
-            restoreState = true
-        }
-    }
-    val navigateToDetails: (PokemonItemEntity) -> Unit = {
-        navController.navigate(PokemonAppDestinations.DETAILS) {
-            popUpTo(navController.graph.findStartDestination().id) {
-                saveState = true
-            }
-            launchSingleTop = true
-            restoreState = true
-        }
-    }
 }
